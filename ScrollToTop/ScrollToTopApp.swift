9d8bd9cb44a677fe5eb0c95dd1fe322a09f443b6
D. Prameswara <meshwara@gmail.com>
2023-07-29 11:09:48 +0700

    Initial Commit

---
+++ ScrollToTop/ScrollToTopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollToTopApp.swift
+//  ScrollToTop
+//
+//  Created by D. Prameswara on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollToTopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
